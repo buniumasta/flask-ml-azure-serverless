6876c9ce51811df13d6956ff67766005bdb7c9c0
Bartosz Ostrowski <buniumasta@gmail.com>
2021-01-07 19:11:12 +0100

    Locust Editing

--- locustfile.py
+++ locustfile.py
@@ -1,8 +1,18 @@
 from locust import HttpUser, task, between
 
 class WebsiteTestUser(HttpUser):
+    wait_time = between(0.5, 3.0)
+
+    def on_start(self):
+        """ on_start is called when a Locust start before any task is scheduled """
+        pass
+
+    def on_stop(self):
+        """ on_stop is called when the TaskSet is stopping """
+        pass
+
     @task(1)
-    def hello_world(self):
+    def predict(self):
         self.client.post("https://flask-ml-myservice.azurewebsites.net:443/predict", {
            "CHAS":{
               "0":0
