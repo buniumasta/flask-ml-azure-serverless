b85c1c0a2e5826faf235383bab98dec2bb1d787d
Bartosz Ostrowski <buniumasta@gmail.com>
2021-01-07 19:20:11 +0100

    Check basic HTTPS

--- locustfile.py
+++ locustfile.py
@@ -12,28 +12,5 @@ class WebsiteTestUser(HttpUser):
         pass
 
     @task(1)
-    def predict(self):
-        self.client.post("https://flask-ml-myservice.azurewebsites.net:443/predict", {
-           "CHAS":{
-              "0":0
-           },
-           "RM":{
-              "0":6.575
-           },
-           "TAX":{
-              "0":296.0
-           },
-           "PTRATIO":{
-              "0":15.3
-           },
-           "B":{
-              "0":396.9
-           },
-           "LSTAT":{
-              "0":4.98
-           }
-        })
-
-    @task(2)
     def index(self):
-        self.client.get("https://flask-ml-myservice.azurewebsites.net/index")
+        self.client.get("http://flask-ml-myservice.azurewebsites.net/index")
